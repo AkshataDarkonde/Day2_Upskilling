da1b9d242b2c3bb0660390659f531bf3e2aa02a8
AkshataDarkonde <91557235+AkshataDarkonde@users.noreply.github.com>
2023-11-21 21:08:43 +0530

    Address by Id commit

---
+++ SpringBoot4/src/main/java/com/Upskilling/SpringBoot4/AddressRepository.java
@@ -0,0 +1,12 @@
+package com.Upskilling.SpringBoot4;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class AddressRepository {
+    public static void main(String[] args) {
+        SpringApplication.run(Address.class, args);
+    }
+
+}
